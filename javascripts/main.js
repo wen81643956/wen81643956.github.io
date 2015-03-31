667016f1831d115ea3bbe930782ca16cc8810628
wen <123456@qq.com>
2015-03-31 17:36:50 +0800

    Revert "ddd"
    This reverts commit 745fef60d67b30701a512ba37706f88715d3ad19.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,9 @@
 'use strict';
 
 window.onload = function () {
-  var container = document.getElementById('container-img');
+  var container  =  document.getElementById('container-img');
   var list = document.getElementById('list');
   var buttons = document.getElementById('buttons');
   var prev = document.getElementById('prev');
   var next = document.getElementById('next');
-
-  next.onclick = function () {
-    list.style.left = parseInt(list.style.left) - 600 + 'px';
-    console.log(list.style.left);
-  };
 };
