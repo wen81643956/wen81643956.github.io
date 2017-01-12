d0ad695ce8dccaf88dfec5b38a0118bee1ebb6de
bowendeng <bowendeng@bowendeng.snsshop.net>
2017-01-12 21:49:39 +0800

    add function

--- javascripts/danmu.js
+++ javascripts/danmu.js
@@ -1,7 +1,15 @@
-var time = '';
 var context = document.getElementById('context');
+var t1 = document.getElementById('t1');
 var position = 800;
+var speed = 50;
 
-time = setInterval(function () {
+function marquee() {
+  if(t1.style.left.replace('px','') < -30) {
+    clearInterval(time);
+  }else {
+    console.log(t1.style.left);
+    t1.style.left = t1.style.left.replace('px','') - 5 + 'px';
+  }
+}
+var time = setInterval(marquee,speed);
 
-}, 100);
